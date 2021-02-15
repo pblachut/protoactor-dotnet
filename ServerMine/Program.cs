28e5294ebecee5fff865317f57fdd1c6d8c10ab1
Grigoris Balaskas <balaskas.greg@gmail.com>
2021-02-15 15:03:07 +0200

    wip

---
+++ ServerMine/Program.cs
@@ -0,0 +1,21 @@
+﻿using Proto;
+using Proto.Remote;
+
+namespace ServerMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = new ActorSystem();
+
+            var serialization = new Serialization();
+            var context = new RootContext(system);
+            serialization.RegisterFileDescriptor(ChatReflection.Descriptor);
+            var remote = new Remote(system, serialization);
+            remote.Start("127.0.0.1", 8000);
+
+            var clients = new HashSet<PID>();
+        }
+    }
+}
